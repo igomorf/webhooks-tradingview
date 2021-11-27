fae180a80fff667747b5542b6593dd2baedf8086
igomorf <78572922+igomorf@users.noreply.github.com>
2021-11-27 19:59:00 -0300

    Commit inicial
    arquivos iniciais do projeto

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, request, abort, json
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
+
+@app.route("/", methods=['GET'])
+def index():
+    return "<h1>Olá Mundo!</h1>"
+
+def main():
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
+
+# @app.route("/webhook", methods=['POST'])
+# def webhook():
+#     if request.method == 'POST':
+#         print(request.json)
+#         return 'success', 200
+#     else:
+#         abort(400)
+
+if __name__  == '__main__':
+    app.run(debug=True)
+
---
+++ webhook.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+webhook_url = 'http://127.0.0.1:5000/webhook'
+
+data = { 'Corretora' : 'exchange',
+         'Preço' : 'BTC',
+         'Operação' : 'Compra' }
+
+r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})

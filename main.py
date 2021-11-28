bbb2e07d93b25844ef1ed23d6bd047f2956866e1
igomorf <78572922+igomorf@users.noreply.github.com>
2021-11-27 23:29:01 -0300

    commit Binance
    Várias linhas do vídeo do Larry

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import os
+import json
+import sys
+import config
+from binance.client import Client
+from binance.enums import *
 from flask import Flask, request, abort, json
 from flask_cors import CORS
 
 
 app = Flask(__name__)
 
+client = Client(config.API_KEY, config.API_SECRET, tld='us')
 cors = CORS(app, resource={r"/*":{"origins": "*"}})
+
+def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
+    try:
+        print(f"enviando a ordem... {order_type} - {side} {quantity} {symbol}")
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+    except Exception as e:
+        print("ocorreu uma exceção de erro - {}".format(e))
+        return False
+
+    return order
 
 @app.route("/", methods=['GET'])
 def index():
@@ -13,7 +29,31 @@ def index():
 
 @app.route("/webhook", methods=['GET'])
 def deploy():
-    return "<h1>Testando deploy do webhook!</h1>"
+
+    data = json.loads(request.data)
+
+    if data['senha'] != config.WEBHOOK_PASSFRASE:
+        return {
+            "code": "Erro",
+            "message": "Senha incorreta, tente novamente!"
+        }
+    print(data['ticker'])
+    print(data['bar'])
+
+    side = data['strategy']['order_action'].upper()
+    quantidade = data['strategy']['order+contracts']
+    order_response = order(side, quantidade, "BTCBUSD")
+    if order_response:
+        return {
+            "code": "Ok",
+            "message": "Ordem executada"
+        }
+    else:
+        print('Ocorreu uma falha com a ordem!')
+        return {
+            "code": "Erro",
+            "message": "Falha na ordem"
+        }
 
 def main():
     port = int(os.environ.get('PORT', 5000))
